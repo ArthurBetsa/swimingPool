c289aaf9aa26b1b586137077a13d9d9674ae5e7a
Arthur <arthur.betsa@gmail.com>
2020-12-25 17:00:00 +0200

    add slider

--- js/jquery/header.controller.js
+++ js/jquery/header.controller.js
@@ -8,6 +8,12 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         items: 1,
+        nav: true,
+        navText: [
+            `<img class="arrow_left arrow_slider" src = "img/main/slider/arrow_next.png" alt="right">`,
+            `<img class="arrow_right arrow_slider" src = "img/main/slider/arrow_next.png" alt="right">`,
+
+        ],
     });
 });
 
